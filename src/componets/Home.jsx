c364db958fe758c09a98bac8801aa43bd2bfb803
Ajeet Singh <ajeet@example.com>
2025-09-16 04:19:52 +0530

    Added validation to prevent blank paste

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -9,39 +9,39 @@ const Home = () => {
     const [value, setValue] = useState("");
     const [searchparams, setparams] = useSearchParams("");
     const PastedId = searchparams.get("pasteId");
-   const dispatch=useDispatch();
-   const allpaste=useSelector((state)=>state.Paste.Pastes)
+    const dispatch = useDispatch();
+    const allpaste = useSelector((state) => state.Paste.Pastes)
 
-useEffect(()=>{
-if(PastedId){
-const paste=allpaste.find((p)=>p._id===PastedId)
-setTile(paste.title)
-setValue(paste.content)
-}
-} ,[PastedId])
+    useEffect(() => {
+        if (PastedId) {
+            const paste = allpaste.find((p) => p._id === PastedId)
+            setTile(paste.title)
+            setValue(paste.content)
+        }
+    }, [PastedId])
 
-    function Createyournotes(){
-        const Paste={
-            title:title,
-            content:value,
-            _id:PastedId|| Date.now().toString(25),
+    function Createyournotes() {
+        const Paste = {
+            title: title,
+            content: value,
+            _id: PastedId || Date.now().toString(25),
             createdAt: new Date().toLocaleString()
         }
-        if(PastedId){
-        dispatch(updatetopaste(Paste));
+        if (PastedId) {
+            dispatch(updatetopaste(Paste));
         }
         if (!title.trim() || !value.trim()) {
-      alert("Title and Content cannot be empty!");
-      return;
-       }
-        else{
-        dispatch(addtopaste(Paste));
+            alert("Title and Content cannot be empty!");
+            return;
+        }
+        else {
+            dispatch(addtopaste(Paste));
         }
 
-    //after creation and updation
-    setTile("");
-    setValue("");
-    setparams({});
+        //after creation and updation
+        setTile("");
+        setValue("");
+        setparams({});
 
     }
 
@@ -49,20 +49,20 @@ setValue(paste.content)
         <div className='flex flex-col '>
 
 
-            <div  className=' '>
+            <div className=' '>
                 <input
                     type='text ' placeholder="Enter your title here" className='border-2  rounded-md  p-2.5  text-center bg-white text-black'
                     value={title}
                     onChange={(e) => setTile(e.target.value)}
                 >
                 </input>
-                <button 
-                
-                onClick={Createyournotes}
-                className='border-2 border-gray-400 rounded-md  m-5  text-center p-2 bg-blue-400 text-white font-bold cursor-pointer'>
+                <button
+
+                    onClick={Createyournotes}
+                    className='border-2 border-gray-400 rounded-md  m-5  text-center p-2 bg-blue-400 text-white font-bold cursor-pointer'>
                     {
                         PastedId ? "Update your notes" : "Create new notes"
-                         
+
                     }
                 </button>
             </div>
